0b6abe968e8a026160057ef852f1b8687dea7a3e
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-06 12:56:20 -0500

    Update 2.11.5 Line of Increasing Blocks.py

--- Introduction to Programming With Turtle Graphics/2.11.5 Line of Increasing Blocks.py
+++ Introduction to Programming With Turtle Graphics/2.11.5 Line of Increasing Blocks.py
@@ -1,8 +1,9 @@
 penup()
 speed()
 setposition(-150, 0)
+x = 10
 # Takes in a size parameter for the length of each side, and an amount
-# paramter for the amount of squares to draw.
+# parameter for the amount of squares to draw.
 def draw_square(size, amount):
     for i in range(amount):
         pendown()
@@ -12,5 +13,5 @@ def draw_square(size, amount):
         penup()
         forward(size * 2)
         size += 10
-
-draw_square(10, 5)
+    
+draw_square(x, 5)

d0be88efb17485f9dd951900e6cd441d6ed5575e
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-08 18:43:53 -0500

    Create 3.6.5 Add Comments!.py

---
+++ Basic Python and Console Interaction/3.6.5 Add Comments!.py
@@ -0,0 +1,12 @@
+"""
+This program asks for your full name then prints it back to you.
+"""
+
+# Asks for your names
+first_name = input("Enter your first name: ")
+middle_name = input("Enter your middle name: ")
+last_name = input("Enter your last name: ")
+
+# Prints your names back to you
+full_name = first_name + " " + middle_name + " " + last_name
+print(full_name)

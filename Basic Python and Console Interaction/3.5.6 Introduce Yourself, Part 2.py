bebe98f6fb2139e22564bffdf18d2ebb1ece87b0
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-08 16:44:32 -0500

    Create 3.5.6 Introduce Yourself, Part 2.py

---
+++ Basic Python and Console Interaction/3.5.6 Introduce Yourself, Part 2.py
@@ -0,0 +1,9 @@
+"""
+stores your name in one variable called name
+stores your age in another variable called age
+prints a one-line greeting that includes your name and your age.
+"""
+name = "Brady"
+age = 17
+
+print("Hello! my name is " + name + " and I am " + str(age) + " years old.")

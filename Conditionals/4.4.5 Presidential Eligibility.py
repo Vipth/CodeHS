e79887679312a67f1f41f80d64839c6fd0dd780a
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-08 20:16:58 -0500

    Update 4.4.5 Presidential Eligibility.py

--- Conditionals/4.4.5 Presidential Eligibility.py
+++ Conditionals/4.4.5 Presidential Eligibility.py
@@ -6,10 +6,15 @@ or not.
 # Gets the users information
 age = int(input("Age: "))
 citizen = input("Born in the U.S.? (Yes/No): ")
-years_of_residency = str(input("Years of Residency: "))
+years_of_residency = int(input("Years of Residency: "))
+
+if citizen.lower() == "yes":
+    citizen = True
+elif citizen.lower() == "no":
+    citizen = False
 
 # Compares their information to the requirements set by the country.
-if age >= 35 and citzien and years_of_residency >= 14:
+if age >= 35 and citizen and years_of_residency >= 14:
     print("You are eligible to run for president!")
 else:
     print("You are not eligible to run for president.")

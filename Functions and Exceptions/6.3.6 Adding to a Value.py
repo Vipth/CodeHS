ae91622348d6e4636d75281e630b98d6801c93ab
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-10 13:12:53 -0500

    Create 6.3.6 Adding to a Value.py

---
+++ Functions and Exceptions/6.3.6 Adding to a Value.py
@@ -0,0 +1,12 @@
+"""
+This program takes a user entered value and adds it to a value 
+taken from the program.
+"""
+
+num1 = 10
+# Your code here...
+num2 = int(input("Enter a number: "))
+def add():
+    sum = num1 + num2
+    print(sum)
+add()

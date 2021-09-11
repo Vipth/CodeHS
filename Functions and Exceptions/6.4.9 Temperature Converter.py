632098f7145630386115b7e185b37ea2f0e8fab6
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-11 17:43:46 -0500

    Update 6.4.9 Temperature Converter.py

--- Functions and Exceptions/6.4.9 Temperature Converter.py
+++ Functions and Exceptions/6.4.9 Temperature Converter.py
@@ -1,25 +1,19 @@
-# Write your function for converting Celsius to Fahrenheit here.
-# Make sure to include a comment at the top that says what
-# each function does!
+# Function for converting Celsius to Farenheit.
 def c_to_f(temp):
     return (1.8 * temp) + 32
-
-
 
 # Now write your function for converting Fahrenheit to Celsius.
 def f_to_c(temp):
     return (temp - 32) / 1.8
 
-
-
-# Now change 0C to F:
+# Changes 0C to F:
 print(c_to_f(0))
 
-# Change 100C to F:
+# Changes 100C to F:
 print(c_to_f(100))
 
-# Change 40F to C:
+# Changes 40F to C:
 print(f_to_c(40))
 
-# Change 80F to C:
+# Changes 80F to C:
 print(f_to_c(80))

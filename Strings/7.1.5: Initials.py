93d5a55318091898ab4a4776ef64b168a04cd015
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-13 09:30:00 -0500

    Create 7.1.5: Initials.py

---
+++ Strings/7.1.5: Initials.py
@@ -0,0 +1,5 @@
+# Return the First and last inital of a user's name.
+def initials(first, last):#  ("Vipth", "AR") --> "V.A."
+    return first[0] + '.' + last[0] + '.' 
+    
+print(initials("Vipth", "AR"))

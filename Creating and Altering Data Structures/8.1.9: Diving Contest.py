2fb617051f04e4f7fe33503908d6d45ba328366c
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-16 09:17:17 -0500

    Update 8.1.9: Diving Contest.py

--- Creating and Altering Data Structures/8.1.9: Diving Contest.py
+++ Creating and Altering Data Structures/8.1.9: Diving Contest.py
@@ -2,3 +2,9 @@
 def calculate_score(judge_scores):
     score = judge_scores[0] + judge_scores[1] + judge_scores[2]
     return score
+
+calculate_score((10, 10, 10))
+# => 30
+
+calculate_score((9, 9, 6))
+# => 24

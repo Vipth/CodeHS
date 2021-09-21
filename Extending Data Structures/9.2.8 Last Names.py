c3badfa1161dccd3a5bb46841119277e8ff3fc8f
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-21 08:59:55 -0500

    Create 9.2.8 Last Names.py

---
+++ Extending Data Structures/9.2.8 Last Names.py
@@ -0,0 +1,10 @@
+names = [
+    "Maya Angelou",
+    "Chimamanda Ngozi Adichie",
+    "Tobias Wolff",
+    "Sherman Alexie",
+    "Aziz Ansari"
+]
+
+# Your code here...
+print([x.split()[-1] for x in names])

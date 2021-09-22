1345fc736f2881a4d84d1fb4c1687f44ab0bf2ff
Vipth <30879538+Vipth@users.noreply.github.com>
2021-09-22 09:25:21 -0500

    Create 10.1.4 Guess the Word, Part 3.py

---
+++ Project: Guess The Word/10.1.4 Guess the Word, Part 3.py
@@ -0,0 +1,47 @@
+secret_word = "Python"
+dashes = '-' * len(secret_word)
+guesses_left = 10
+win = None
+
+def get_guess():
+    while True:
+        guess = input("Guess: ")
+        if len(guess) == 1 and type(guess) == str and guess.islower() == True:
+            return guess
+        else:
+            print("Must be a single lowercase letter!")
+            
+def update_dashes(secret_word, dashes, guess):
+    for i in range(len(secret_word)):
+        dashes = list(dashes)
+        if guess == secret_word[i]:
+            if i == 0:
+                dashes[i] = guess.upper()
+            else:
+                dashes[i] = guess
+    dashes = ''.join(dashes)
+    return dashes
+
+while True:
+    print(dashes)
+    
+    if dashes.lower() == secret_word.lower():
+        win = True
+    if guesses_left == 0:
+        win = False
+    if win == True:
+        print("Congratulations! You Win.")
+        break
+    elif win == False:
+        print(f"You lose. The word was: {secret_word}.")
+        break
+    
+    print(f"{guesses_left} guesses left.")
+    guess = get_guess()
+    
+    if guess not in secret_word.lower():
+        guesses_left -= 1
+        print(f"That letter is not in the word!")
+    else:
+        print("That letter is in the word.")
+        dashes = update_dashes(secret_word.lower(), dashes, guess)

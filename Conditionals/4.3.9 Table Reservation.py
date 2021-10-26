d7893eb8e497bf72fdec72cf42cd7af4718de64b
Vipth <30879538+Vipth@users.noreply.github.com>
2021-10-26 09:33:52 -0500

    Update 4.3.9 Table Reservation.py

--- Conditionals/4.3.9 Table Reservation.py
+++ Conditionals/4.3.9 Table Reservation.py
@@ -2,7 +2,7 @@ name = input("What is your name: ")
 
 reservation_name = "Shonda"
 
-if name == reservation_name.lower():
+if name.lower() == reservation_name.lower():
     print("Right this way!")
 else:
     print("Sorry, we don't have a reservation under that name.")

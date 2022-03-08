cf40a967fcbfada01f55b722de8f1965d31d399c
Vipth <30879538+Vipth@users.noreply.github.com>
2022-03-08 09:29:11 -0600

    Create 4.3.5 Rolling Dice.py

---
+++ AP CS Programming/4.3.5 Rolling Dice.py
@@ -0,0 +1,8 @@
+first_die = int(input("First Die? "))
+print(first_die)
+
+second_die = int(input("Second Die? "))
+print(second_die)
+
+rolled_doubles = first_die == second_die
+print(f"Rolled Doubles? {rolled_doubles}")

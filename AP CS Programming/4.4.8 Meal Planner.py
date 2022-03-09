e19c470574a2a6a8ce3cc3c86843bcbd68e0344b
Vipth <30879538+Vipth@users.noreply.github.com>
2022-03-09 09:41:42 -0600

    Create 4.4.8 Meal Planner.py

---
+++ AP CS Programming/4.4.8 Meal Planner.py
@@ -0,0 +1,8 @@
+meal = input("Breakfast, lunch, or dinner? ").lower()
+
+if meal == "breakfast":
+    print("How about some cereal?")
+elif meal == "lunch":
+    print("How about some ramen?")
+elif meal == "dinner":
+    print("How about some ribs?")

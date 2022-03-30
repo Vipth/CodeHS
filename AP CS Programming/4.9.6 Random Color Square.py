c48094641f04d77485a76cc6dbb17d17be27231e
Vipth <30879538+Vipth@users.noreply.github.com>
2022-03-30 09:21:50 -0500

    Create 4.9.6 Random Color Square.py

---
+++ AP CS Programming/4.9.6 Random Color Square.py
@@ -0,0 +1,17 @@
+from random import choice
+
+set_size(480, 400)
+
+# This graphics program should draw a square. 
+# The fill color should be randomly choosen from
+# the COLORS list
+
+SIDE_LENGTH = 100
+COLORS = [Color.red, Color.orange, Color.yellow, Color.green, Color.blue, 
+        Color.purple, Color.black, Color.gray]
+        
+
+sq = Rectangle(SIDE_LENGTH, SIDE_LENGTH)
+sq.set_position(get_width() / 2, get_height() / 2)
+sq.set_color(choice(COLORS))
+add(sq)

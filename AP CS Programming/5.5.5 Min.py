7e896945ff68b63c912e547e20da5ef6300f10f0
Vipth <30879538+Vipth@users.noreply.github.com>
2022-05-16 08:59:42 -0500

    Create 5.5.5 Min.py

---
+++ AP CS Programming/5.5.5 Min.py
@@ -0,0 +1,9 @@
+def minVal(num1, num2) -> int:
+    return min(num1, num2)
+    
+x = minVal(10, 14)
+print(f"The min is {x}")
+x = minVal(20, -15)
+print(f"The min is {x}")
+x = minVal(36, 56)
+print(f"The min is {x}")
